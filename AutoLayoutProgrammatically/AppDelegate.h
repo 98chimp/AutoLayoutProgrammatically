94b13bc4b63eda0c28f24181174ac19d8277989d
Shahin Zangenehpour <98chimp@users.noreply.github.com>
2015-03-22 17:00:37 -0700

    Initial Commit

---
+++ AutoLayoutProgrammatically/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutProgrammatically
+//
+//  Created by Shahin on 2015-03-22.
+//  Copyright (c) 2015 98% Chimp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayoutProgrammatically/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutProgrammatically
+//
+//  Created by Shahin on 2015-03-22.
+//  Copyright (c) 2015 98% Chimp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
